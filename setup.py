3462630fa7e188989aedd7f702f1714b28adb4c9
lcdr <lcdr@localhost>
2019-05-17 11:57:59 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os.path
+
+from setuptools import setup
+
+setup(
+	name="utils",
+	version="0.1.0",
+	description="Utilities for working with LEGO Universe file formats and network packets.",
+	author="lcdr",
+	url="https://bitbucket.org/lcdr/utils/",
+	license="AGPL v3",
+	packages=["utils"],
+	python_requires=">=3.6",
+)
